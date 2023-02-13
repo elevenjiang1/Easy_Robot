7221ead101768dd7ed15617ea9ce51409398862a
elevenjiang1 <1206413225@qq.com>
2023-02-14 00:50:59 +0800

    Finish record data

--- ur_robot.py
+++ ur_robot.py
@@ -2,71 +2,115 @@ import os
 import sys
 import urx
 import time
+import datetime
+from pathlib import Path
 import cv2 as cv
 import numpy as np
 
 import signal
 import threading
-#For canceling signal
+# For canceling signal
 signal.signal(signal.SIGINT, quit)
 signal.signal(signal.SIGTERM, quit)
 
+Abs_Path = os.path.dirname(os.path.abspath(__file__))
 
 
 class UR_Robot:
-    def __init__(self,robot_ip):
-        self.robot_ip=robot_ip
-        self._robot=urx.Robot(self.robot_ip)
-        
-        self._q=None
-        self._x=None
-        
-        
-        robot_state_thread=threading.Thread(target=self.robot_state_cb)
-        robot_state_thread.start()
-        
+    def __init__(self, robot_ip):
+        self.robot_ip = robot_ip
+        self._robot = urx.Robot(self.robot_ip)
+
+        self._q = None
+        self._x = None
+
+        self.robot_state_thread = threading.Thread(target=self.robot_state_cb)
+        self.robot_state_thread.start()
+
         while self._q is None:
             print("Wait for robots joints...")
             time.sleep(1)
-            
+
         print("UR robot: {} is ready!".format(self.robot_ip))
-        
-        
+
     def robot_state_cb(self):
         while True:
-            self._q=self._robot._get_joints_dist()
-            self._x=self._robot.get_pose()
-            
-            time.sleep(0.008)#Set in 125hz
-        
+            self._q = self._robot.getj()
+            self._x = self._robot.getl()
+
+            time.sleep(0.008)  # Set in 125hz
+
     def get_robot_state(self):
-        return self._q,self._x
-    
-    
+        return self._q, self._x
+
+
 def read_images():
-    camera=cv.VideoCapture(0)
+    camera = cv.VideoCapture(2)
     while True:
-        begin_time=time.time()
-        ret,frame=camera.read()
-        
-        cv.imshow("image",frame)
-        
+        begin_time = time.time()
+        ret, frame = camera.read()
+
+        # cv.imshow("image",frame)
+
         print("camera hz is:{}".format(1/(time.time()-begin_time)))
+
+        # key_input=cv.waitKey(1)
+        # if key_input==ord('q'):
+        #     break
+
+
+def basic_get_robot():
+    robot = UR_Robot(robot_ip="192.168.10.101")
+
+    while True:
+        robot_joints, robot_pose = robot.get_robot_state()
+        print("Robot new state is:")
+        print(robot_joints)
+        print(robot_pose)
+        time.sleep(1)
+
+
+def record_data():
+    # 1: create save data
+    timestr = str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M'))
+    save_path = os.path.join(Abs_Path, "data/{}".format(timestr))
+    if os.path.exists(save_path):
+        print("!!!!!!!!Error, exist save path!!!!!!!!!!!!!!")
+        # return
+    else:
+        os.mkdir(save_path)
+
+    # 2: init robot and camera
+    robot = UR_Robot(robot_ip="192.168.10.101")
+    camera = cv.VideoCapture(2)
+    save_number = 0
+    pose_save_list = []
+    while True:
+        begin_time = time.time()
+        ret, frame = camera.read()
+        robot_joints, robot_pose = robot.get_robot_state()
+
+        cv.imwrite(os.path.join(
+            save_path, "color_{}.png".format(save_number)), frame)
+        pose_save_list.append(robot_pose)
+
         
-        key_input=cv.waitKey(1)
-        if key_input==ord('q'):
+        cv.imshow("Record_image", frame)
+        key_input =cv.waitKey(1)
+        if key_input == ord('q'):
             break
         
-def basic_get_robot():
-    robot=UR_Robot(robot_ip="192.168.0.1")
-    robot_joints,robot_pose=robot.get_robot_state()
-    print(robot_joints)
-    print(robot_pose)
-    
+        print("record hz is:{}".format(1/(time.time()-begin_time)))
 
+    save_array = np.array(pose_save_list)
 
+    np.savez(os.path.join(save_path, "all_poses.npz"), all_poses=save_array)
+
+    print("Finish save {} data".format(save_array.shape[0]))
+    robot.robot_state_thread.stop()
 
 
 if __name__ == "__main__":
-    read_images()
+    # read_images()
     # basic_get_robot()
+    record_data()
